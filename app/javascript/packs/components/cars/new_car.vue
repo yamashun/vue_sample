8be743f3c66c40670b78fb42a244308c675f4aa5
山田 俊輔 <shunsuke.yamada@glv.co.jp>
2018-03-29 22:15:38 +0900

    コンポーネント追加・修正

--- app/javascript/packs/components/cars/new_car.vue
+++ app/javascript/packs/components/cars/new_car.vue
@@ -1,35 +1,33 @@
 // html
 <template>
-  <div class="section">
-    <div class="container">
-      <h1 class="title">メーカーと車種を選択してください</h1>
-      <div class="box">
-        <div class="field">
-          <label class="label">メーカー: {{ car.maker.name }} </label>
-          <div class="control">
-            <div class="select is-medium">
-              <select v-model="car.maker" class="CarModelSelectList" @change="fetchModels">
-                <option v-for="maker in makers" v-bind:value="maker">
-                    {{ maker.name }}
-                </option>
-              </select>
-            </div>
+  <div class="container">
+    <h1 class="title">メーカーと車種を選択してください</h1>
+    <div class="box">
+      <div class="field">
+        <label class="label">メーカー: {{ car.maker.name }} </label>
+        <div class="control">
+          <div class="select is-medium">
+            <select v-model="car.maker" class="CarModelSelectList" @change="fetchModels">
+              <option v-for="maker in makers" v-bind:value="maker">
+                  {{ maker.name }}
+              </option>
+            </select>
           </div>
         </div>
-        <div class="field">
-          <label class="label">車種: {{ car.model ? car.model.name : "" }}</label>
-          <div class="control">
-            <div class="select is-medium">
-              <select v-model="car.model" class="CarModelSelectList" >
-                <option v-for="model in models" v-bind:value="model">
-                    {{ model.name }}
-                </option>
-              </select>
-            </div>
-          </div>
-        </div>
-        <button class="button is-link is-outlined" v-on:click="nextPage()">次へ</button>
       </div>
+      <div class="field">
+        <label class="label">車種: {{ car.model ? car.model.name : "" }}</label>
+        <div class="control">
+          <div class="select is-medium">
+            <select v-model="car.model" class="CarModelSelectList" >
+              <option v-for="model in models" v-bind:value="model">
+                  {{ model.name }}
+              </option>
+            </select>
+          </div>
+        </div>
+      </div>
+      <button class="button is-info is-outlined" v-on:click="nextPage()">次へ</button>
     </div>
   </div>
   
@@ -51,6 +49,11 @@
     },
     mounted: function () {
       this.fetchMakers();
+      this.car.maker = this.$store.getters.maker_obj;
+      if(this.car.maker.id) {
+        this.fetchModels();
+        this.car.model = this.$store.getters.model_obj;
+      }
     },
     methods: {
       fetchMakers: function () {
@@ -68,7 +71,7 @@
           });
       },
       nextPage: function() {
-        // this.$store.commit('setMaker', this.car);
+        this.$store.commit('setCar', this.car);
         this.$router.push('/cars/new_price');
       },
     },
--- app/javascript/packs/components/cars/new_price.vue
+++ app/javascript/packs/components/cars/new_price.vue
@@ -1,17 +1,15 @@
 <template>
-  <div class="section">
-    <div class="container">
-      <h1 class="title">価格を入力してください</h1>
-      <div class="box">
-        <div class="field">
-          <label class="label">価格: {{ price }} </label>
-          <div class="control">
-            <input type="number" class="input is-primary" v-model="price">
-          </div>
+  <div class="container">
+    <h1 class="title">価格を入力してください</h1>
+    <div class="box">
+      <div class="field">
+        <label class="label">価格: {{ price }} </label>
+        <div class="control">
+          <input type="number" class="input is-primary" v-model="price">
         </div>
-        <button class="button is-link is-outlined" v-on:click="previousPage()">戻る</button>
-        <button class="button is-link is-outlined" v-on:click="inputConfirm()">確認</button>
       </div>
+      <button class="button is-warning is-outlined" v-on:click="previousPage()">戻る</button>
+      <button class="button is-info is-outlined" v-on:click="inputConfirm()">確認</button>
     </div>
   </div>
 </template>
@@ -23,13 +21,16 @@
         price: "",
       }
     },
+    mounted: function () {
+      this.price = this.$store.getters.price;
+    },
     methods: {
       inputConfirm: function() {
-        // this.$store.commit('setPrice', this.price);
-        // this.$router.push('/cars/confirm');
+        this.$store.commit('setPrice', this.price);
+        this.$router.push('/cars/confirm');
       },
       previousPage: function() {
-        // this.$store.commit('setPrice', this.price);
+        this.$store.commit('setPrice', this.price);
         this.$router.push('/cars/new');
       }
     },
